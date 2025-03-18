f4dc5046e2a63eb0417b436d2cc19b29d4f4bf13
chandan84 <61125092+chandan84@users.noreply.github.com>
2025-03-18 09:05:22 -0400

    Update custom_headers.py

--- middlewares/custom_headers.py
+++ middlewares/custom_headers.py
@@ -1,1 +1,15 @@
+# middlewares/custom_headers.py
+from fastapi import Request, Response
+from starlette.middleware.base import BaseHTTPMiddleware
 
+class CustomHeadersMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next) -> Response:
+        response = await call_next(request)
+        
+        # Add custom headers to response
+        response.headers["X-Proxy-Service"] = "CLINE-LiteLLM-Proxy"
+        response.headers["X-Model-Source"] = request.headers.get(
+            "X-Model-Source", "default"
+        )
+        
+        return response
